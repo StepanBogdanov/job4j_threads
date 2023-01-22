d9d39bd56602fd26fbfb8deee9540f3a9332130e
Stepan Bogdanov <bogstepan@gmail.com>
2023-01-22 14:30:58 +0300

    3. Состояние нити. [#229175]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,10 +3,10 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> {}
+                () -> { }
         );
         Thread second = new Thread(
-                () -> {}
+                () -> { }
         );
         System.out.printf("%s: %s%n", first.getName(), first.getState());
         System.out.printf("%s: %s%n", second.getName(), second.getState());
