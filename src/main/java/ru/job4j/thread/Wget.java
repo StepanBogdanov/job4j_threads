ba74c8e3aaeae7d45168b34cf6be387429539eb8
Stepan Bogdanov <bogstepan@gmail.com>
2023-01-25 13:04:44 +0300

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -1,34 +1,44 @@
 package ru.job4j.thread;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Paths;
 
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String path;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String path) {
         this.url = url;
         this.speed = speed;
+        this.path = path;
     }
 
     @Override
     public void run() {
+        String fileName = url.substring(url.lastIndexOf('/') + 1);
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("readme.txt")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(path + fileName)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
-            long currentTime = System.currentTimeMillis();
+            int downloadData = 0;
+            long start = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                downloadData += bytesRead;
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                long executeTime = System.currentTimeMillis() - currentTime;
-                if (executeTime < speed) {
-                    Thread.sleep(speed - executeTime);
+                if (downloadData >= speed) {
+                    long executeTime = System.currentTimeMillis() - start;
+                    if (executeTime < 1000) {
+                        Thread.sleep(1000 - executeTime);
+                    }
+                    downloadData = 0;
+                    start = System.currentTimeMillis();
                 }
-                currentTime = System.currentTimeMillis();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -41,14 +51,16 @@ public class Wget implements Runnable {
         validate(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String path = args[2];
+        Thread wget = new Thread(new Wget(url, speed, path));
         wget.start();
         wget.join();
     }
 
     private static void validate(String[] args) {
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Введите параметры: URL, максимальная скорость загрузки (кБ/с)");
+        if (args.length < 3) {
+            throw new IllegalArgumentException("Введите параметры: URL, максимальная скорость загрузки (Б/с),"
+                    + "путь для загрузки.");
         }
         try {
             new URL(args[0]);
@@ -61,7 +73,10 @@ public class Wget implements Runnable {
                 throw new IllegalArgumentException("Скорость загрузки должна быть больше 0");
             }
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Введите максимальную скорость загрузки (кБ/с)");
+            throw new IllegalArgumentException("Введите максимальную скорость загрузки (Б/с)");
+        }
+        if (!new File(args[2]).isDirectory()) {
+            throw new IllegalArgumentException("Введите корректный путь для зазрузки файла");
         }
     }
 }
