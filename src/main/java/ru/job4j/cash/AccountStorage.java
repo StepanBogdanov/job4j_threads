8aa15ee86d92a4e803567fff09c16dd21c065552
Stepan Bogdanov <bogstepan@gmail.com>
2023-02-03 16:33:58 +0300

    3. Денежные переводы AccountStorage [#1104]

---
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -0,0 +1,41 @@
+package ru.job4j.cash;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+@ThreadSafe
+public class AccountStorage {
+    @GuardedBy("this")
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+
+    public synchronized boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) != null;
+    }
+
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), account) != null;
+    }
+
+    public synchronized boolean delete(int id) {
+        return accounts.remove(id) != null;
+    }
+
+    public Optional<Account> getById(int id) {
+        return Optional.ofNullable(accounts.get(id));
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        Optional<Account> fromAccount = getById(fromId);
+        Optional<Account> toAccount = getById(toId);
+        if (fromAccount.isPresent() && toAccount.isPresent() && fromAccount.get().amount() >= amount) {
+            update(new Account(fromId, fromAccount.get().amount() - amount));
+            update(new Account(toId, toAccount.get().amount() + amount));
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
