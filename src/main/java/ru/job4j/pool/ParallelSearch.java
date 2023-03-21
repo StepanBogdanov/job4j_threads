be0edb1d6029411fac32ddff4171c63557a066cf
Stepan Bogdanov <bogstepan@gmail.com>
2023-03-21 21:18:12 +0300

    3. ForkJoinPool [#315067 #402904]

--- src/main/java/ru/job4j/pool/ParallelSearch.java
+++ src/main/java/ru/job4j/pool/ParallelSearch.java
@@ -5,12 +5,12 @@ import java.util.concurrent.RecursiveTask;
 
 public class ParallelSearch<V> extends RecursiveTask<Integer> {
 
-    private final Object[] array;
+    private final V[] array;
     private final V value;
     private int from;
     private int to;
 
-    public ParallelSearch(Object[] array, V value, int from, int to) {
+    public ParallelSearch(V[] array, V value, int from, int to) {
         this.array = array;
         this.value = value;
         this.from = from;
@@ -20,7 +20,7 @@ public class ParallelSearch<V> extends RecursiveTask<Integer> {
     @Override
     protected Integer compute() {
         if (to - from < 10) {
-            return linearSearch(array, value);
+            return linearSearch();
         }
         int mid = (from + to) / 2;
         ParallelSearch<V> leftSearch = new ParallelSearch(array, value, from, mid);
@@ -29,22 +29,16 @@ public class ParallelSearch<V> extends RecursiveTask<Integer> {
         rightSearch.fork();
         int left = leftSearch.join();
         int right = rightSearch.join();
-        if (left != -1) {
-            return left;
-        } else if (right != -1) {
-            return right;
-        } else {
-            return -1;
-        }
+        return Math.max(left, right);
     }
 
-    public static <V> Integer find(Object[] array, V value) {
+    public static <V> Integer find(V[] array, V value) {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        return forkJoinPool.invoke(new ParallelSearch<V>(array, value, 0, array.length - 1));
+        return forkJoinPool.invoke(new ParallelSearch<>(array, value, 0, array.length - 1));
     }
 
-    private int linearSearch(Object[] array, V value) {
-        for (int i = 0; i < array.length; i++) {
+    private int linearSearch() {
+        for (int i = from; i <= to; i++) {
             if (array[i].equals(value)) {
                 return i;
             }
