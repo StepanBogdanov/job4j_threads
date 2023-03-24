89b218f20a84ec7a2bf08a1ca5ff6b1b6e75a43e
Stepan Bogdanov <bogstepan@gmail.com>
2023-03-24 13:21:43 +0300

    4. CompletableFuture [#361626 #403771]

--- src/test/java/ru/job4j/pool/RolColSumTest.java
+++ src/test/java/ru/job4j/pool/RolColSumTest.java
@@ -13,25 +13,23 @@ class RolColSumTest {
     @Test
     public void whenSeqSum() {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        RolColSum.Sums[] sums = sum(matrix);
-        assertThat(sums[0].getRowSum()).isEqualTo(6);
-        assertThat(sums[0].getColSum()).isEqualTo(12);
-        assertThat(sums[1].getRowSum()).isEqualTo(15);
-        assertThat(sums[1].getColSum()).isEqualTo(15);
-        assertThat(sums[2].getRowSum()).isEqualTo(24);
-        assertThat(sums[2].getColSum()).isEqualTo(18);
+        Sums[] sums = sum(matrix);
+        assertThat(sums).isEqualTo(new Sums[] {
+                new Sums(6, 12),
+                new Sums(15, 15),
+                new Sums(24, 18)
+        });
     }
 
     @Test
     public void whenAsyncSum() throws ExecutionException, InterruptedException {
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        RolColSum.Sums[] sums = asyncSum(matrix);
-        assertThat(sums[0].getRowSum()).isEqualTo(6);
-        assertThat(sums[0].getColSum()).isEqualTo(12);
-        assertThat(sums[1].getRowSum()).isEqualTo(15);
-        assertThat(sums[1].getColSum()).isEqualTo(15);
-        assertThat(sums[2].getRowSum()).isEqualTo(24);
-        assertThat(sums[2].getColSum()).isEqualTo(18);
+        Sums[] sums = asyncSum(matrix);
+        assertThat(sums).isEqualTo(new Sums[] {
+                new Sums(6, 12),
+                new Sums(15, 15),
+                new Sums(24, 18)
+        });
     }
 
 }
